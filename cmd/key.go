1bee4d0b3d212c20e013562e1fc18240dcbb943a
Mikael Berthe <mikael@lilotux.net>
2018-03-09 22:20:25 +0100

    samtvcli key: Accept several keys
    Also, the special arg '_' adds a pause.

--- cmd/key.go
+++ cmd/key.go
@@ -23,6 +23,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -36,21 +37,24 @@ var keyList *bool
 
 // keyCmd represents the key command
 var keyCmd = &cobra.Command{
-	Use:   "key KEY",
+	Use:   "key KEY...",
 	Short: "Send a key to the TV",
-	Long: `Send a key code.
+	Long: `Send one or several key codes to the TV device.
 
-The available key identifiers can be displayed using the --list option.`,
+The available key identifiers can be displayed using the --list option.
+
+When several keys are given, a small delay is inserted between the
+keys.  If a bigger pause is required, the special argument '_' can be used.`,
 	Example: `  samtvcli key --list
   samtvcli key KEY_VOLDOWN
   samtvcli key KEY_MENU
   samtvcli key KEY_DOWN
   samtvcli key KEY_RETURN
-  samtvcli key KEY_POWEROFF`,
+  samtvcli key KEY_POWEROFF
+  samtvcli key KEY_MENU _ _ KEY_DOWN KEY_DOWN _ KEY_UP _ KEY_UP _ KEY_RETURN`,
 	Args: func(cmd *cobra.Command, args []string) error {
-		if !*keyList && len(args) != 1 {
-			// TODO: allow several keys
-			return fmt.Errorf("accepts 1 arg, received %d", len(args))
+		if !*keyList && len(args) < 1 {
+			return fmt.Errorf("requires at least 1 arg or --list")
 		}
 		return nil
 	},
@@ -68,8 +72,20 @@ The available key identifiers can be displayed using the --list option.`,
 			os.Exit(1)
 		}
 
-		if err := samtvSession.Key(args[0]); err != nil {
-			logrus.Error("Cannot send key: ", err)
+		for i, k := range args {
+			// Special argument '_' is a pause
+			if k == "_" {
+				time.Sleep(400 * time.Millisecond)
+				continue
+			}
+			if err := samtvSession.Key(k); err != nil {
+				logrus.Errorf("Cannot send key '%s': %v", k, err)
+				os.Exit(1)
+			}
+			// Add a small pause between several keys
+			if i+1 < len(args) {
+				time.Sleep(100 * time.Millisecond)
+			}
 		}
 		samtvSession.Close()
 	},
